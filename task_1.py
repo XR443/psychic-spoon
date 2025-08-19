4fe77fb845f00e283e0ff4a256415269ddb75597
Maksim Raspopov <hardlolmaster@bk.ru>
2025-08-19 19:07:17 +0500

    Задания по занятию 14

--- task_1.py
+++ task_1.py
@@ -1,29 +1,12 @@
-import random
+def recursive_print(vals):
+    def index_print(vals, index):
+        if 0 <= index < len(vals):
+            print(vals[index], end=' ')
+            index_print(vals, index - 1)
+
+    index_print(vals, len(vals) - 1)
+    print("Конец списка")
 
 
-def generate_matrix(n, m):
-    rand = random.Random()
-    result = []
-    for i in range(n):
-        result.append([rand.randint(-10, 10) for _ in range(m)])
-
-    return result
-
-
-def sum_matrix(matrix_l, matrix_r):
-    if len(matrix_l) != len(matrix_r) and len(matrix_l[0]) != len(matrix_r[0]):
-        raise ValueError("Матрицы должны быть одной размерности")
-
-    n = len(matrix_l)
-
-    result = []
-    for i in range(n):
-        result.append([x + y for x, y in zip(matrix_l[i], matrix_r[i])])
-
-    return result
-
-
-matrix_1 = generate_matrix(10, 10)
-matrix_2 = generate_matrix(10, 10)
-
-print(sum_matrix(matrix_1, matrix_2))
+my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+recursive_print(my_list)
