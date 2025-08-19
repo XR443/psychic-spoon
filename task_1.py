8124d25b77d097a5aad02e170be5196917c9f1c2
Maksim Raspopov <hardlolmaster@bk.ru>
2025-08-19 19:15:04 +0500

    Задания по занятию 15

--- task_1.py
+++ task_1.py
@@ -1,12 +1,5 @@
-def recursive_print(vals):
-    def index_print(vals, index):
-        if 0 <= index < len(vals):
-            print(vals[index], end=' ')
-            index_print(vals, index - 1)
+from classes import Transport, Autobus
 
-    index_print(vals, len(vals) - 1)
-    print("Конец списка")
-
-
-my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-recursive_print(my_list)
+autobus = Transport("Renault Logan", 180, 12)
+print(autobus.seating_capacity(5))
+print(autobus)
---
+++ task_2.py
@@ -0,0 +1,5 @@
+from classes import Transport, Autobus
+
+autobus = Autobus("Renault Logan", 180, 12)
+print(autobus.seating_capacity())
+print(autobus)
