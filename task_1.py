a1cbb0f132d3df7b0a441d10c5a97ae9ada8d518
Maksim Raspopov <hardlolmaster@bk.ru>
2025-08-19 19:01:05 +0500

    Задания по занятию 13

--- task_1.py
+++ task_1.py
@@ -1,13 +1,29 @@
-def factorial(n):
-    if n < 0:
-        raise ValueError("Факториал определен только для неотрицательных чисел")
-    if n == 0 or n == 1:
-        return 1
-    result = 1
-    for i in range(2, n + 1):
-        result *= i
+import random
+
+
+def generate_matrix(n, m):
+    rand = random.Random()
+    result = []
+    for i in range(n):
+        result.append([rand.randint(-10, 10) for _ in range(m)])
+
     return result
 
-start_num = int(input("Введите число: "))
 
-print([factorial(num) for num in range(factorial(start_num), 0, -1)])
+def sum_matrix(matrix_l, matrix_r):
+    if len(matrix_l) != len(matrix_r) and len(matrix_l[0]) != len(matrix_r[0]):
+        raise ValueError("Матрицы должны быть одной размерности")
+
+    n = len(matrix_l)
+
+    result = []
+    for i in range(n):
+        result.append([x + y for x, y in zip(matrix_l[i], matrix_r[i])])
+
+    return result
+
+
+matrix_1 = generate_matrix(10, 10)
+matrix_2 = generate_matrix(10, 10)
+
+print(sum_matrix(matrix_1, matrix_2))
